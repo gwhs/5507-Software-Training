fdcb8fea281b25558a1ebab20ba1dde1c44d834d
Isaac Ng <isaacngkh@gmail.com>
2025-09-18 21:35:40 -0700

    started advanced commands

---
+++ 18-Advanced Commands and Trigger Bindings - Copy/src/main/java/frc/robot/subsystems/endEffector/EndEffectorConstants.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems.endEffector;
+
+public class EndEffectorConstants {
+
+  public static final int deviceID = 16;
+  public static final int GEAR_RATIO = 3;
+  public static final double HOLD_CORAL_CURRENT = 10.0;
+  public static final int CORAL_DETECTOR_ID = 2; // Assume 0 for now
+  public static final double EXP_DECAY = 0.02; // exponential decay rate for EMA
+  public static final double SDEV_DECAY = 0.02;
+  public static final double INTAKE_CORAL_CURRENT = 30;
+  public static final double VOLTAGE_L4 = 9;
+  public static final double VOLTAGE_L3 = 5;
+  public static final double VOLTAGE_L2 = 5;
+  public static final double VOLTAGE_L1 = 2.5;
+}
