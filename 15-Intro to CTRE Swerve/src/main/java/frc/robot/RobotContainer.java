79d5f87dc8cfae8b0d2a008c3c2505e306c812eb
Isaac Ng <isaacngkh@gmail.com>
2025-09-21 19:46:15 -0700

    intro to ctre swerve

---
+++ 15-Intro to CTRE Swerve/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,29 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.swerve.SwerveSubsystem;
+import frc.robot.subsystems.swerve.TunerConstants_Anemone;
+
+public class RobotContainer {
+  private final SwerveSubsystem swerve = TunerConstants_Anemone.createDrivetrain();
+
+  private final CommandXboxController controller = new CommandXboxController(0);
+
+  public RobotContainer() {
+    configureBindings();
+  }
+
+  private void configureBindings() {
+    swerve.setDefaultCommand(swerve.defaultDrive(controller));
+  }
+
+  public Command getAutonomousCommand() {
+    return Commands.print("No autonomous command configured");
+  }
+}
