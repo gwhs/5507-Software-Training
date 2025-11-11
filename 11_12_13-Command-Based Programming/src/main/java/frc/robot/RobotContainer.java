f0a99717d8967bf80afb4162bca3d6b7a696fd91
Owen Kuo <owennkuo@gmail.com>
2025-11-10 17:49:40 -0800

    keybinds to left and right bumper

--- 11_12_13-Command-Based Programming/src/main/java/frc/robot/RobotContainer.java
+++ 11_12_13-Command-Based Programming/src/main/java/frc/robot/RobotContainer.java
@@ -6,9 +6,12 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.RobotVisualizer;
+import frc.robot.subsystems.arm.ArmConstants;
 import frc.robot.subsystems.arm.ArmSubsystem;
+import frc.robot.subsystems.elevator.ElevatorConstants;
 import frc.robot.subsystems.elevator.ElevatorSubsystem;
 import frc.robot.subsystems.endEffector.EndEffectorSubsystem;
 import frc.robot.subsystems.groundIntake.GroundIntakeSubsystem;
@@ -41,12 +44,11 @@ public class RobotContainer {
     controller.x().onTrue(elevator.runHeight(0));
 
     // TODO 1: press left bumper: set arm angle to 120
-
+    controller.leftBumper().onTrue(arm.runAngle(120));
     // TODO 2: press right bumper -> set arm angle to -90
-
+    controller.leftBumper().onTrue(arm.runAngle(-90));
     // TODO 3: press start -> score L4 Coral
     // ↓↓↓↓↓↓↓↓↓↓ COMPLETE THE COMMAND COMPOSITION IN scoreL4Coral() METHOD BELOW FIRST ↓↓↓↓↓↓↓↓↓↓
-
   }
 
   public Command getAutonomousCommand() {
@@ -68,6 +70,10 @@ public class RobotContainer {
         //     Command 4a: retract elevator to ElevatorConstants.STOW_METER
         //     Command 4b: rotate arm to ArmConstants.ARM_STOW_ANGLE
         //     Command 4c: spin endeffector at 0 volts
+        Commands.parallel(
+          elevator.runHeight(ElevatorConstants.L4_PREP_POSITION),
+          arm.runAngle(ArmConstants.L4_PREP_POSITION)
+        )
         );
   }
 }
