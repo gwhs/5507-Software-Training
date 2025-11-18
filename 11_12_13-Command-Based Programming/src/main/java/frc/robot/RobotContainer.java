306632b61918e9b3f5df0fe89dbd28252f3a0c5a
Owen Kuo <owennkuo@gmail.com>
2025-11-17 17:03:30 -0800

    12 Intermediate Command-Based Programming, fix key bindings

--- 11_12_13-Command-Based Programming/src/main/java/frc/robot/RobotContainer.java
+++ 11_12_13-Command-Based Programming/src/main/java/frc/robot/RobotContainer.java
@@ -4,7 +4,9 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.RobotVisualizer;
@@ -31,6 +33,8 @@ public class RobotContainer {
 
   public RobotContainer() {
     configureBindings();
+
+    SmartDashboard.putData("Command Scheduler", CommandScheduler.getInstance());
   }
 
   private void configureBindings() {
@@ -39,17 +43,18 @@ public class RobotContainer {
     /********************
      * Button Bindings
      ********************/
-    controller.a().onTrue(elevator.runHeight(0.3));
     controller.b().onTrue(elevator.runHeight(.75));
     controller.x().onTrue(elevator.runHeight(0));
 
     // TODO 1: press left bumper: set arm angle to 120
     controller.leftBumper().onTrue(arm.runAngle(120));
     // TODO 2: press right bumper -> set arm angle to -90
-    controller.leftBumper().onTrue(arm.runAngle(-90));
+    controller.rightBumper().onTrue(arm.runAngle(-90));
     // TODO 3: press start -> score L4 Coral
     // ↓↓↓↓↓↓↓↓↓↓ COMPLETE THE COMMAND COMPOSITION IN scoreL4Coral() METHOD BELOW FIRST ↓↓↓↓↓↓↓↓↓↓
-    controller.start().onTrue(scoreL4Coral());
+    controller.start().and(controller.back()).debounce(1).onTrue(scoreL4Coral());
+    controller.start().and(controller.back().negate()).debounce(1).onTrue(arm.runAngle(90));
+    controller.back().and(controller.start().negate().debounce(1).onTrue(arm.runAngle(-80)));
   }
 
   public Command getAutonomousCommand() {
@@ -61,24 +66,26 @@ public class RobotContainer {
    ********************/
   public Command scoreL4Coral() {
     return Commands.sequence(
-        // TODO:
-        // Command 1: In parallel: (Commands.parallel())
-        //     Command 1a: extend elevator to ElevatorConstants.L4_PREP_POSITION
-        //     Command 1b: rotate arm to ArmConstants.L4_PREP_POSITION
-        // Command 2: Spin endeffector at EndEffectorConstants.VOLTAGE_L4
-        // Command 3: Wait 0.05 seconds (Commands.waitSeconds())
-        // Command 4: In parallel:
-        //     Command 4a: retract elevator to ElevatorConstants.STOW_METER
-        //     Command 4b: rotate arm to ArmConstants.ARM_STOW_ANGLE
-        //     Command 4c: spin endeffector at 0 volts
-        Commands.parallel(
-            elevator.runHeight(ElevatorConstants.L4_PREP_POSITION),
-            arm.runAngle(ArmConstants.L4_PREP_POSITION)),
-        endEffector.runVoltage(EndEffectorConstants.VOLTAGE_L4),
-        Commands.waitSeconds(0.05),
-        Commands.parallel(
-            elevator.runHeight(ElevatorConstants.STOW_METER),
-            arm.runAngle(ArmConstants.ARM_STOW_ANGLE),
-            endEffector.runVoltage(0)));
+            // TODO:
+            // Command 1: In parallel: (Commands.parallel())
+            //     Command 1a: extend elevator to ElevatorConstants.L4_PREP_POSITION
+            //     Command 1b: rotate arm to ArmConstants.L4_PREP_POSITION
+            // Command 2: Spin endeffector at EndEffectorConstants.VOLTAGE_L4
+            // Command 3: Wait 0.05 seconds (Commands.waitSeconds())
+            // Command 4: In parallel:
+            //     Command 4a: retract elevator to ElevatorConstants.STOW_METER
+            //     Command 4b: rotate arm to ArmConstants.ARM_STOW_ANGLE
+            //     Command 4c: spin endeffector at 0 volts
+            Commands.parallel(
+                    elevator.runHeight(ElevatorConstants.L4_PREP_POSITION),
+                    arm.runAngle(ArmConstants.L4_PREP_POSITION))
+                .withTimeout(0.5),
+            endEffector.runVoltage(EndEffectorConstants.VOLTAGE_L4),
+            Commands.waitSeconds(0.05),
+            Commands.parallel(
+                elevator.runHeight(ElevatorConstants.STOW_METER),
+                arm.runAngle(ArmConstants.ARM_STOW_ANGLE),
+                endEffector.runVoltage(0).withTimeout(0.01)))
+        .withName("ScoreL4Coral");
   }
 }
