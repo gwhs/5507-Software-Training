87a89d86a0fe13746a4cc450791f825cf404c977
nicoella bartholoemew <Niliang1@s.sfusd.edu>
2026-01-07 15:55:58 -0800

    .

--- 23-Phoenix 6 and Subsystem/src/main/java/frc/robot/subsystems/DummySubsystem.java
+++ 23-Phoenix 6 and Subsystem/src/main/java/frc/robot/subsystems/DummySubsystem.java
@@ -4,9 +4,14 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
 
 import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj.Alert.AlertType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -17,9 +22,29 @@ public class DummySubsystem extends SubsystemBase {
   private final TalonFX motor = new TalonFX(1, "rio");
 
   public DummySubsystem() {
+
+    TalonFXConfiguration talonFXConfig = new TalonFXConfiguration();
+
+    //talonFXConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+    talonFXConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+
+    talonFXConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+    talonFXConfig.CurrentLimits.StatorCurrentLimit = 10;
+
     SmartDashboard.putData("Run Motor at 1V", runVoltage(1));
     SmartDashboard.putData("Run Motor at 0V", runVoltage(0));
     SmartDashboard.putData("Run Motor at -1V", runVoltage(-1));
+
+    StatusCode status = StatusCode.StatusCodeNotInitialized;
+    for (int i = 0; i < 5; i++)
+    {
+      status = motor.getConfigurator().apply(talonFXConfig);
+      if (status.isOK()) break;
+    }
+    if (!status.isOK())
+    {
+      new Alert("Could not configure device. Error: " + status.toString(), AlertType.kError).set(true);
+    }
   }
 
   @Override
