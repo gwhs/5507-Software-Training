fe774f2afefd075637f54bf5a992e3cf355ca843
Caden <Cachen15@s.sfusd.edu>
2026-01-07 16:02:50 -0800

    changes

--- 11_12_13-Command-Based Programming/src/main/java/frc/robot/RobotContainer.java
+++ 11_12_13-Command-Based Programming/src/main/java/frc/robot/RobotContainer.java
@@ -8,8 +8,11 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.RobotVisualizer;
+import frc.robot.subsystems.arm.ArmConstants;
 import frc.robot.subsystems.arm.ArmSubsystem;
+import frc.robot.subsystems.elevator.ElevatorConstants;
 import frc.robot.subsystems.elevator.ElevatorSubsystem;
+import frc.robot.subsystems.endEffector.EndEffectorConstants;
 import frc.robot.subsystems.endEffector.EndEffectorSubsystem;
 import frc.robot.subsystems.groundIntake.GroundIntakeSubsystem;
 import frc.robot.subsystems.swerve.SwerveSubsystem;
@@ -41,10 +44,11 @@ public class RobotContainer {
     controller.x().onTrue(elevator.runHeight(0));
 
     // TODO 1: press left bumper: set arm angle to 120
-
+    controller.leftBumper().onTrue(arm.runAngle(120));
     // TODO 2: press right bumper -> set arm angle to -90
-
-    // TODO 3: press start -> score L4 Coral
+    controller.rightBumper().onTrue(arm.runAngle(-90));
+       // TODO 3: press start -> score L4 Coral
+       controller.start().onTrue(scoreL4Coral());
     // ↓↓↓↓↓↓↓↓↓↓ COMPLETE THE COMMAND COMPOSITION IN scoreL4Coral() METHOD BELOW FIRST ↓↓↓↓↓↓↓↓↓↓
 
   }
@@ -60,14 +64,24 @@ public class RobotContainer {
     return Commands.sequence(
         // TODO:
         // Command 1: In parallel: (Commands.parallel())
-        //     Command 1a: extend elevator to ElevatorConstants.L4_PREP_POSITION
+      Commands.parallel(
+        elevator.runHeight(ElevatorConstants.L4_PREP_POSITION),
+        arm.runAngle(ArmConstants.L4_PREP_POSITION),
+      endEffector.runVoltage(EndEffectorConstants.VOLTAGE_L4)),
+      Commands.waitSeconds(0.05),
+      Commands.parallel(
+      elevator.runHeight(ElevatorConstants.STOW_METER),
+      arm.runAngle(ArmConstants.ARM_STOW_ANGLE),
+      endEffector.runVoltage(0))
+     //     Command 1a: extend elevator to ElevatorConstants.L4_PREP_POSITION
         //     Command 1b: rotate arm to ArmConstants.L4_PREP_POSITION
         // Command 2: Spin endeffector at EndEffectorConstants.VOLTAGE_L4
         // Command 3: Wait 0.05 seconds (Commands.waitSeconds())
         // Command 4: In parallel:
         //     Command 4a: retract elevator to ElevatorConstants.STOW_METER
         //     Command 4b: rotate arm to ArmConstants.ARM_STOW_ANGLE
+      
         //     Command 4c: spin endeffector at 0 volts
-        );
+    );
   }
 }
