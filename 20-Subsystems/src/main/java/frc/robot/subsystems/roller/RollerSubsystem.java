c0a189d33a121c6490651fbaf64f483d434ca97d
isaacngkh <isaacngkh@gmail.com>
2026-05-22 13:59:32 -0700

    test

---
+++ 20-Subsystems/src/main/java/frc/robot/subsystems/roller/RollerSubsystem.java
@@ -0,0 +1,74 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.roller;
+
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Temperature;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj.Alert.AlertType;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class RollerSubsystem extends SubsystemBase {
+  private final TalonFX motor;
+
+  private final Alert motorNotConnectedAlert = new Alert("Roller Motor Not Connected", AlertType.kError);
+
+  private final VelocityVoltage VelocityRequest = new VelocityVoltage(0);
+
+  private final StatusSignal<Voltage> motorVoltage;
+  private final StatusSignal<Temperature> motorTemp;
+  private final StatusSignal<Current> motorStatorCurrent;
+
+  /** Creates a new ShooterSubsystem. */
+  public RollerSubsystem(CANBus canBus) {
+    motor = new TalonFX(40, canBus);
+
+    TalonFXConfiguration config = new TalonFXConfiguration();
+
+    config.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+    config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+
+    config.CurrentLimits.StatorCurrentLimitEnable = true;
+    config.CurrentLimits.StatorCurrentLimit = 40;
+
+    config.Slot0.kS = 0.1;
+    config.Slot0.kV = 0.1125;
+    config.Slot0.kP = 2;
+
+    motor.getConfigurator().apply(config);
+
+    motorVoltage = motor.getMotorVoltage();
+    motorTemp = motor.getDeviceTemp();
+    motorStatorCurrent = motor.getStatorCurrent();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    motorNotConnectedAlert.set(!motor.isConnected());
+  }
+
+  public Command runVoltage(double volt) {
+    return this.runOnce(() -> {
+      motor.setVoltage(volt);
+    });
+  }
+
+  public Command runVelocity (double rps) {
+    return this.runOnce(() -> {
+      motor.setControl(VelocityRequest.withVelocity(rps));
+    });
+  }
+}
---
+++ 20-Subsystems/src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
@@ -0,0 +1,74 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.shooter;
+
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj.Alert.AlertType;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ShooterSubsystem extends SubsystemBase {
+  private final TalonFX motor;
+
+  private final Alert motorNotConnectedAlert = new Alert("Shooter Motor Not Connected", AlertType.kError);
+
+  private final VelocityVoltage request = new VelocityVoltage(0);
+
+  /** Creates a new ShooterSubsystem. */
+  public ShooterSubsystem(CANBus canBus) {
+    motor = new TalonFX(21, canBus);
+
+    TalonFXConfiguration config = new TalonFXConfiguration();
+
+    config.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+    config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+
+    config.CurrentLimits.StatorCurrentLimitEnable = true;
+    config.CurrentLimits.StatorCurrentLimit = 10;
+
+    config.Slot0.kS = 0;
+    config.Slot0.kV = 0.1125;
+    config.Slot0.kP = 0;
+
+    motor.getConfigurator().apply(config);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    motorNotConnectedAlert.set(!motor.isConnected());
+  }
+
+  public Command startShooter() {
+    return this.runOnce(() -> {
+      motor.setVoltage(1);
+    });
+  }
+
+  public Command stopShooter() {
+    return this.runOnce(() -> {
+      motor.setVoltage(0);
+    });
+  }
+
+  public Command runVoltage(double volt) {
+    return this.runOnce(() -> {
+      motor.setVoltage(volt);
+    });
+  }
+
+  public Command runVelocity (double rps) {
+    return this.runOnce(() -> {
+      motor.setControl(request.withVelocity(rps));
+    });
+  }
+}
