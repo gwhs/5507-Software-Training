cb35be9824ab1ac6f1569a7d846e10655c8ac2d1
isaacngkh <isaacngkh@gmail.com>
2026-05-24 12:34:39 -0700

    removed import

--- 20-Subsystems/src/main/java/frc/robot/RobotContainer.java
+++ 20-Subsystems/src/main/java/frc/robot/RobotContainer.java
@@ -4,8 +4,6 @@
 
 package frc.robot;
 
-import com.ctre.phoenix6.CANBus;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
