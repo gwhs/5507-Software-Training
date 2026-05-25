dc4504ece117291abc5aad15525ae789567d6e35
isaacngkh <isaacngkh@gmail.com>
2026-05-24 17:17:55 -0700

    more

--- 20-Subsystems/src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
+++ 20-Subsystems/src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
@@ -4,13 +4,19 @@
 
 package frc.robot.subsystems.shooter;
 
+import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
+import dev.doglog.DogLog;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Temperature;
+import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.Alert.AlertType;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -20,6 +26,10 @@ public class ShooterSubsystem extends SubsystemBase {
   private final TalonFX motor;
 
   private final Alert motorNotConnectedAlert = new Alert("Shooter Motor Not Connected", AlertType.kError);
+
+  private final StatusSignal<Voltage> motorVoltage;
+  private final StatusSignal<Temperature> motorTemp;
+  private final StatusSignal<Current> motorStatorCurrent;
 
   private final VelocityVoltage request = new VelocityVoltage(0);
 
@@ -40,12 +50,24 @@ public class ShooterSubsystem extends SubsystemBase {
     config.Slot0.kP = 0;
 
     motor.getConfigurator().apply(config);
+
+    motorVoltage = motor.getMotorVoltage();
+    motorTemp = motor.getDeviceTemp();
+    motorStatorCurrent = motor.getStatorCurrent();
+
+    BaseStatusSignal.setUpdateFrequencyForAll(50, motorVoltage, motorTemp, motorStatorCurrent);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
     motorNotConnectedAlert.set(!motor.isConnected());
+
+    BaseStatusSignal.refreshAll(motorVoltage, motorTemp, motorStatorCurrent);
+
+    DogLog.log("Shooter/Motor Voltage", motorVoltage.getValueAsDouble());
+    DogLog.log("Shooter/Motor Temp", motorTemp.getValueAsDouble());
+    DogLog.log("Shooter/Stator Current", motorStatorCurrent.getValueAsDouble());
   }
 
   public Command startShooter() {
