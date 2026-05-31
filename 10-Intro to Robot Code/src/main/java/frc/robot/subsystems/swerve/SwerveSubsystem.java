d5963afaa55a70ac2bb80d154faf7914dd81b6c5
Isaac Ng <isaacngkh@gmail.com>
2026-05-30 22:02:12 -0700

    combine 10

--- 10-Intro to Robot Code/src/main/java/frc/robot/subsystems/swerve/SwerveSubsystem.java
+++ 10-Intro to Robot Code/src/main/java/frc/robot/subsystems/swerve/SwerveSubsystem.java
@@ -2,6 +2,8 @@ package frc.robot.subsystems.swerve;
 
 import static edu.wpi.first.units.Units.MetersPerSecond;
 
+import java.util.function.Supplier;
+
 import com.ctre.phoenix6.Utils;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
@@ -24,7 +26,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commands.AlignToPose;
-import java.util.function.Supplier;
 
 /**
  * Class that extends the Phoenix 6 SwerveDrivetrain class and implements Subsystem so it can easily
